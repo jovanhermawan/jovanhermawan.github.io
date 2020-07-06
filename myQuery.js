47b5d7f92eba3395e00b0cef6b34acf573819bae
Jovan Hermawan <hermawan.jovan20@gmail.com>
2020-07-06 23:39:10 +0800

    phone optimization 2

--- myQuery.js
+++ myQuery.js
@@ -1,10 +1,12 @@
+if($(window).width() >= 1024){
 $(document).ready(function(){
 		$("#top").fadeIn(1000, function(){
 	$("#topcont").animate({
 		top: "400px"
 	},"slow");
-});
-		});
+});});
+}
+
 
 $(document).ready(function(){
 				$("#tes").hover(function(){
