90bad952f54af7c4a8aa2cd4b5c72d71015a8493
Jovan Hermawan <hermawan.jovan20@gmail.com>
2020-07-06 22:16:06 +0800

    optimization 2

--- myQuery.js
+++ myQuery.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
-		$("#top").fadeIn(2000);
+		$("#top").fadeIn(2000, function(){
+	$("#topcont").animate({
+		top: "400px"
+	},"slow");
+});
 		});
 
 $(document).ready(function(){
 				$("#tes").hover(function(){
 					$("#tes").animate({
 						fontSize: "60px"
-					}, "slow");});})
+					}, "slow"); });})
+
+
+
