0fbac4cb75cbbc955ec1ee2ded3ffe06937da253
Jovan Hermawan <hermawan.jovan20@gmail.com>
2020-07-06 17:45:17 +0800

    more queries

--- myScript.js
+++ myScript.js
@@ -7,7 +7,7 @@ var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset > sticky) {
+  if (window.pageYOffset > sticky+600) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
